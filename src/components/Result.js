3179977c7631c22991e384cd36ef4b641626cc18
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-10-11 16:07:44 +0530

    print Image and upload on AWS

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,8 +2,8 @@
 import { v4 as uuid } from 'uuid';
 import QRCode from 'qrcode.react'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Modal } from 'react-bootstrap'
 import { useRouter } from 'next/navigation'
 import { FaHome } from 'react-icons/fa'
 import axios from 'axios';
@@ -11,6 +11,7 @@ const Result = ({ result, setResult }) => {
     const unique_id = uuid();
     const small_id = unique_id.slice(0, 5)
     const router = useRouter()
+    const [qr, setQr] = useState('')
 
     const handleRedirect = () => {
         setTimeout(() => {
@@ -18,44 +19,92 @@ const Result = ({ result, setResult }) => {
         }, 5000)
     }
 
-    const [qr, setQr] = useState('')
-    const handleQrCode = async () => {
-        // setQr(result)
+    const handleUploadImage = async () => {
         try {
             const data = await axios.post('https://dis2023.com/aiphotobooth/upload.php', {
-                img: result
+                img: result.split(',')[1]
             })
-            console.log(data)
+            setQr(data?.data?.url)
         } catch (error) {
             console.log(error)
-
         }
     }
+    useEffect(() => {
+        handleUploadImage()
+    }, [])
+    const [showQr, setShowQr] = useState(false)
+    const handleQrCode = async () => {
+        if (qr) {
+            setShowQr(true)
+        } else {
+            const isSave = await handleUploadImage()
+            if (isSave) {
+                setShowQr(true)
+            }
+        }
+    }
+
+    const handlePrint = () => {
+        const printWindow = window.open('', '_blank');
+        printWindow.document.write(`
+            <html>
+                <head>
+                    <title>Print</title>
+                </head>
+            <body>
+                <div id="finalImag"> 
+                    <img src=${result} style="width:100%; border-radius :10px"/> 
+                </div>
+            </body>
+            </html>    
+        `);
+        printWindow.document.close();
+        printWindow.print();
+        printWindow.close();
+    };
     return (
         <div className='center_main py-5 '>
             <h1 className='text-center'>Here is Your Photograph</h1>
             <Container className=''>
                 <Row className='justify-content-center align-items-center'>
                     <Col lg={9} md={12} sm={12} xs={12}>
-                        <div className="finalImag">
+                        <div className="finalImag" id='finalImag'>
                             <img src={result} alt="" />
                         </div>
-                        <div className="d-flex justify-content-between py-4">
-                            <div>
+                        <div className="d-flex justify-content-between flex-wrap  py-4" >
+                            <div className='my-2'>
                                 <button className='btn btn-warning wt-border start-btn' onClick={() => setResult('')}>Re-generate</button>
                             </div>
-                            <div>
-                                {/* <button onClick={handleRedirect} className='btn wt-border btn-warning start-btn'>download</button> */}
+                            <div className='my-2'>
+                                <button onClick={handleQrCode} className='btn wt-border btn-warning start-btn'>Geberate QR</button>
+                            </div>
+                            <div className='my-2'>
                                 <a href={result} download={`${small_id}`} target="_blank" rel="noopener noreferrer" className='btn wt-border btn-warning start-btn'>Save</a>
                             </div>
-                            {/* <div>
-                                <button onClick={handleQrCode} className='btn wt-border btn-warning start-btn'>Geberate QR</button>
-                            </div> */}
+                            <div className='my-2'>
+                                <button onClick={handlePrint} className='btn wt-border btn-warning start-btn' >print</button>
+                            </div>
                         </div>
                     </Col>
-                    {/* <Col lg={6} md={12} sm={12} xs={12}>
-                        <QRCode value={qr.slice(0, 500)} size={408} />
-                    </Col> */}
+                    <Modal
+                        show={showQr}
+                        onHide={() => { setShowQr(false) }}
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered >
+                        <Modal.Header closeButton>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Scan this QR to get image
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div className="d-flex justify-content-center">
+                                <QRCode value={qr} size={200} />
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button className='btn btn-danger' onClick={() => setShowQr(false)}>Close</button>
+                        </Modal.Footer>
+                    </Modal>
                 </Row>
             </Container>
             <Link href='/' className="back-home">
